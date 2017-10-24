1c1d575cacfdf69cde1a3098caee62a2a6c8d281
liujia <liujia@LiuJia-MacBook-Pro.local>
2017-10-24 16:32:47 +0800

    增加毫秒时间戳到字符串的辅助函数

--- libs/utils/miscellaneous.go
+++ libs/utils/miscellaneous.go
@@ -3,6 +3,7 @@ package utils
 import (
     "time"
     "syscall"
+    "fmt"
 )
 
 func EnableFileDescriptor(limit uint64) (err error) {
@@ -28,4 +29,11 @@ func CurrentTimeMillSecond() int64 {
 
 func CurrentTimeSecond() int64 {
     return time.Now().Unix()
+}
+
+func Time2String(ts int64) string  {
+    var secs int64 = ts/1000
+    var left = ts%1000
+    var s string = time.Unix(secs, 0).Format("2006-01-02 15:04:05")
+    return s+fmt.Sprintf(":%v", left)
 }
